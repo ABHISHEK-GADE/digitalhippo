0c42ae94bbb4b4b0e086916ebcd6012ef60bec57
ABHISHEK-GADE <gadeabhishek71@gmail.com>
2024-05-19 20:09:24 +0530

    added

--- src/get-payload.ts
+++ src/get-payload.ts
@@ -46,7 +46,7 @@ export const getPayloadClient = async ({
     cached.promise = payload.init({
       email: {
         transport: transporter,
-        fromAddress: 'hello@joshtriedcoding.com',
+        fromAddress: 'support@craftedturbo.tech',
         fromName: 'DigitalHippo',
       },
       secret: process.env.PAYLOAD_SECRET,
